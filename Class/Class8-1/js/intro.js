6f9ca080738abfda15d453accc76d549ba24499b
axr1766 <60405004+axr1766@users.noreply.github.com>
2020-03-31 22:20:44 -0400

    tweaked websitess, fixed bugs

---
+++ Class/Class8-1/js/intro.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+
+
+      Alert =   function (){ // now has global scope
+          alert("This is an alert box");
+        };
+
+      //Selecting content
+      $("h1").text("Greetings");
+      //Modifying content
+      // .text("Greetings");
+
+
+      // Reacting on click
+      $(".hi").click(function(){
+          //Modifying content
+          $(".greeting").text("Hi");
+          $(".greeting").css("font-family", "serif");
+
+      });
+
+
+      //Program Rules (like a recipe)
+      $(".hello").click(function(){
+          //Modifying content
+          $(".greeting").text("Hello");
+          $(".greeting").css("font-family", "sans-serif");
+
+      });
+
+
+
+
+      //Program Rules (like a recipe)
+      $(".button").click(function(){
+          //Modifying content
+        $(".grid div:first-child ").css("background-color", "lightblue");
+        $(".grid div:nth-child(2) ").css("background-color", "lightblue");
+        $(".grid div:nth-child(3) ").css("background-color", "lightblue");
+
+      });
+
+      //Program Rules (like a recipe)
+      $(".button2").click(function(){
+          //Modifying content
+        $(".grid div:first-child").css("background-color", "pink");
+        $(".grid div:nth-child(2)").css("background-color", "pink");
+        $(".grid div:nth-child(3)").css("background-color", "pink")
+
+      });
+
+});
