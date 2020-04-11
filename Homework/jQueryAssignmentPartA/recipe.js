27c6f091670d97265c0be0aa98a96c5050afe13b
axr1766 <60405004+axr1766@users.noreply.github.com>
2020-04-10 22:34:16 -0400

    finished jquery website

---
+++ Homework/jQueryAssignmentPartA/recipe.js
@@ -0,0 +1,155 @@
+$(document).ready( function(){
+
+$(".step1").hide()
+ $(".step2").hide()
+ $(".step3").hide()
+ $(".step4").hide()
+ $(".step5").hide()
+
+
+
+
+  $("#_fadein").click( function(){
+    $(".step1 ").fadeIn (3000);
+  });
+
+$("#_fadein2").click( function(){
+  $(".step2 ").fadeIn (3000);
+});
+
+$("#_fadein3").click( function(){
+  $(".step3 ").fadeIn (3000);
+});
+
+$("#_fadein4").click( function(){
+  $(".step4 ").fadeIn (3000);
+});
+
+$("#_fadein5").click( function(){
+  $(".step5 ").fadeIn (3000);
+});
+
+$(".eggs1").mouseover( function(){
+  $(".eggs1").css ("font-size", "150%" )
+  $(".eggs1").css ("font-weight", "bold" )
+    $(".eggs1").css ("color", "red" )
+  $(".eggs1").html ("One, Two or Three, as many as you want");
+  });
+  $(".eggs1").mouseleave( function(){
+    $(".eggs1").css ("font-size", "100%" )
+    $(".eggs1").css ("font-weight", "normal" )
+    $(".eggs1").html ("Eggs");
+    });
+
+    $(".frying-pan").mouseover( function(){
+      $(".frying-pan").css ("font-size", "150%" )
+      $(".frying-pan").css ("font-weight", "bold" )
+        $(".frying-pan").css ("color", "orange" )
+      $(".frying-pan").html ("Make sure its not too big and that it is made of a nonstick material");
+      });
+      $(".frying-pan").mouseleave( function(){
+        $(".frying-pan").css ("font-size", "100%" )
+        $(".frying-pan").css ("font-weight", "normal" )
+        $(".frying-pan").html ("Frying pan");
+        });
+
+        $(".oil").mouseover( function(){
+          $(".oil").css ("font-size", "150%" )
+          $(".oil").css ("font-weight", "bold" )
+            $(".oil").css ("color", "RGB(182, 214, 0)" )
+          $(".oil").html ("Any of your favorite vegeteble oil should be fine:)");
+          });
+          $(".oil").mouseleave( function(){
+            $(".oil").css ("font-size", "100%" )
+            $(".oil").css ("font-weight", "normal" )
+            $(".oil").html ("Oil");
+            });
+
+            $(".salt").mouseover( function(){
+              $(".salt").css ("font-size", "150%" )
+              $(".salt").css ("font-weight", "bold" )
+                $(".salt").css ("color", "RGB(0, 154, 214)" )
+              $(".salt").html ("White salt? pink salt? no salt at all?!!! You are the master of your destiny in this one.");
+              });
+              $(".salt").mouseleave( function(){
+                $(".salt").css ("font-size", "100%" )
+                $(".salt").css ("font-weight", "normal" )
+                $(".salt").html ("Salt");
+                });
+
+                $(".pepper").mouseover( function(){
+                  $(".pepper").css ("font-size", "150%" )
+                  $(".pepper").css ("font-weight", "bold" )
+                    $(".pepper").css ("color", "RGB(121, 0, 168)" )
+                  $(".pepper").html ("C'mon its fried eggs, it doesn't have to be too fancy XD");
+                  });
+                  $(".pepper").mouseleave( function(){
+                    $(".pepper").css ("font-size", "100%" )
+                    $(".pepper").css ("font-weight", "normal" )
+                    $(".pepper").html ("Pepper");
+
+});
+
+                $(".eggpic2").mouseover( function(){
+                $(".eggpic2").css ("max-width", "150%")
+                $(".eggpic2").css ("height", "auto");
+});
+                $(".eggpic2").mouseleave( function(){
+              $(".eggpic2").css ("max-width", "50%");
+            });
+
+
+            $(".eggpic3").mouseover( function(){
+            $(".eggpic3").css ("max-width", "150%")
+            $(".eggpic3").css ("height", "auto");
+          });
+            $(".eggpic3").mouseleave( function(){
+          $(".eggpic3").css ("max-width", "50%");
+          });
+
+
+
+          $(".eggpic4").mouseover( function(){
+          $(".eggpic4").css ("max-width", "150%")
+          $(".eggpic4").css ("height", "auto");
+        });
+          $(".eggpic4").mouseleave( function(){
+        $(".eggpic4").css ("max-width", "50%");
+        });
+
+
+
+
+        $(".eggpic5").mouseover( function(){
+        $(".eggpic5").css ("max-width", "150%")
+        $(".eggpic5").css ("height", "auto");
+      });
+        $(".eggpic5").mouseleave( function(){
+      $(".eggpic5").css ("max-width", "50%");
+      });
+
+
+
+
+      $(".eggpic6").mouseover( function(){
+      $(".eggpic6").css ("max-width", "150%")
+      $(".eggpic6").css ("height", "auto");
+    });
+      $(".eggpic6").mouseleave( function(){
+    $(".eggpic6").css ("max-width", "50%");
+    });
+
+
+
+
+
+    $(".eggpic7").mouseover( function(){
+    $(".eggpic7").css ("max-width", "150%")
+    $(".eggpic7").css ("height", "auto");
+  });
+    $(".eggpic7").mouseleave( function(){
+  $(".eggpic7").css ("max-width", "50%");
+  });
+
+
+                });
